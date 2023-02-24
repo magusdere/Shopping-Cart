afa84ff1ea56fe1172e63aee1dbc5a48ef324eda
Magusdere <maximodecima123@gmail.com>
2023-02-24 18:31:13 -0300

    Created HTML structure and Javascript logic.

---
+++ app.js
@@ -0,0 +1,27 @@
+const carrito = [];
+const ul = document.querySelector(".listaDinamica");
+const fragment = document.createDocumentFragment();
+
+const item = prompt("Que desea comprar?");
+carrito.push(item);
+
+while(confirm("Desea comprar algo mas?")) {
+    const item = prompt("Que desea comprar?");
+    carrito.push(item);
+}
+
+carrito.forEach((item) => {
+    const li = document.createElement("li")
+
+    li.textContent = item; 
+    fragment.appendChild(li);
+});
+
+const tuCarrito= document.querySelector(".tuCarrito");
+const tuCarritoText = document.createElement("span");
+tuCarrito.textContent = "Compraste:";
+
+tuCarrito.appendChild(tuCarritoText)
+
+ul.appendChild(fragment)
+
